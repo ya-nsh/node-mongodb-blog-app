35d8f86a218987cc9071bbe6d531da88b96e5333
ya-nsh <yansh2017@gmail.com>
2022-04-23 22:22:17 +0530

    added settings, login, register pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Settings from './pages/settings/Settings';
 import Single from './pages/single/Single';
 import Write from './pages/write/Write';
 
@@ -8,7 +11,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <Write />
+      <Register />
     </>
   );
 }

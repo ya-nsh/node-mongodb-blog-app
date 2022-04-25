fb9ffd7fd9268bb351ca6ec45186beb3eeb13a8e
ya-nsh <yansh2017@gmail.com>
2022-04-25 20:57:09 +0530

    added a user schema and boilerplates

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log('connected to mongodb'))
+  .catch(err => console.log(err));
 
 app.listen('5000', () => {
   console.log('Server is running on port 5000');
